b67fb7dd8d618f080660b6f45fc105c381b0349d
sekrang <git commit -m first>
2022-05-29 16:17:53 +0900

    Initial Commit

---
+++ codeup-project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  codeup-project
+//
+//  Created by 조성찬 on 2022/05/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
